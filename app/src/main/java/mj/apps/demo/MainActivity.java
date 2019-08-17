36a56d3f00920194b9a6049eca0a218f52a07c04
thaoluuvan <luu.van.thao@framgia.com>
2019-08-17 23:58:14 +0700

    Test block thread

--- app/src/main/java/mj/apps/demo/MainActivity.java
+++ app/src/main/java/mj/apps/demo/MainActivity.java
@@ -5,8 +5,14 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -16,6 +22,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> list = getColorList();
+        Observable<List<String>> observable = Observable.just(list);
+        observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<List<String>>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            
+            }
+            
+            @Override
+            public void onNext(List<String> strings) {
+                Log.d(TAG, "onNext: " + Thread.currentThread());
+                Log.d(TAG, "onNext: " + strings);
+            }
+            
+            @Override
+            public void onError(Throwable e) {
+            
+            }
+            
+            @Override
+            public void onComplete() {
+            
+            }
+        });
     }
     
     public Observer<Integer> getFirstObserver() {
@@ -67,6 +97,22 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         return integerObserver;
+    }
+    
+    private List<String> getColorList() {
+        Log.d(TAG, "getColorList: " + Thread.currentThread());
+        List<String> strings = new ArrayList<>();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 10; i++) {
+            strings.add(i + "");
+        }
+        
+        return strings;
+        
     }
     
 }
