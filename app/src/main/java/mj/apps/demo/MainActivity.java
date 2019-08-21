a13c10cccccfb1bbf57a123f29e6e6ec20f3c2b4
thaoluuvan <luu.van.thao@framgia.com>
2019-08-21 13:04:52 +0700

    Added bottom navigation view

--- app/src/main/java/mj/apps/demo/MainActivity.java
+++ app/src/main/java/mj/apps/demo/MainActivity.java
@@ -1,17 +1,29 @@
 package mj.apps.demo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     
     private static final String TAG = MainActivity.class.getSimpleName();
+    private BottomNavigationView bottomNavigationView;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        });
     }
 }
