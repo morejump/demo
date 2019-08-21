20436874a6f438e4ee8aad8969a9a5efe6b6bc08
thaoluuvan <luu.van.thao@framgia.com>
2019-08-21 15:34:33 +0700

    Added fragment to bottom navigation view

--- app/src/main/java/mj/apps/demo/MainActivity.java
+++ app/src/main/java/mj/apps/demo/MainActivity.java
@@ -5,9 +5,15 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
+import mj.apps.demo.fragments.FirstFragment;
+import mj.apps.demo.fragments.SecondFragment;
+import mj.apps.demo.fragments.ThirdFragment;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -22,8 +28,27 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_bottom_first:
+                        loadFragment(new FirstFragment());
+                        break;
+                    case R.id.nav_bottom_second:
+                        loadFragment(new SecondFragment());
+                        break;
+                    case R.id.nav_bottom_third:
+                        loadFragment(new ThirdFragment());
+                        break;
+                    
+                }
                 return false;
             }
         });
+        
+    }
+    
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmnetHolder, fragment);
+        fragmentTransaction.commit();
     }
 }
