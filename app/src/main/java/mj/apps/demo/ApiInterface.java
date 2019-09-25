619d24628e88debdc63c93f8ad3ea697977c9b7d
thaoluuvan <luu.van.thao@framgia.com>
2019-09-25 18:27:54 +0700

    Test CI

--- app/src/main/java/mj/apps/demo/ApiInterface.java
+++ app/src/main/java/mj/apps/demo/ApiInterface.java
@@ -3,13 +3,14 @@ package mj.apps.demo;
 import java.util.List;
 
 import io.reactivex.Single;
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
-
+    
     @GET("posts")
-    public Single<List<Post>> getPosts();
-
+    Single<List<Post>> getPosts();
+    
     @GET("photos")
-    public Single<List<Photo>> getPhotos();
+    Single<List<Photo>> getPhotos();
 }
--- app/src/main/java/mj/apps/demo/MainActivity.java
+++ app/src/main/java/mj/apps/demo/MainActivity.java
@@ -1,53 +1,53 @@
 package mj.apps.demo;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-import io.reactivex.Single;
-import io.reactivex.SingleObserver;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     private static final String TAG = MainActivity.class.getSimpleName();
-    Button btnClick;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Single<List<Post>> observable = Client.getAPI().getPosts();
-        Single<List<Photo>> observable1 = Client.getAPI().getPhotos();
-
-        Single.zip(observable, observable1, (posts, photos) -> "")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<String>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onSuccess(String aVoid) {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-                });
-
+        
+        Observable.just(1, 2, 3, 4, 5, 6).buffer(2, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(new Observer<List<Integer>>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+            
+            @Override
+            public void onNext(List<Integer> integers) {
+                Log.d(TAG, "onNext: " + integers.toString());
+            }
+            
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+            
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+        
+        // hello CI
+        
     }
-
-    private void getData(List<Post> posts, List<Photo> photos) {
-
-    }
+    
 }
