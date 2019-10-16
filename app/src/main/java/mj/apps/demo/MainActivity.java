ca753d906970deb9b13eee564d051c09579ab6ae
unknown <luuvanthaepu@gmail.com>
2019-10-16 23:09:37 +0700

    hahah

--- app/src/main/java/mj/apps/demo/MainActivity.java
+++ app/src/main/java/mj/apps/demo/MainActivity.java
@@ -1,22 +1,8 @@
 package mj.apps.demo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 
-import com.google.gson.JsonObject;
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-import mj.apps.demo.Models.Post;
-import mj.apps.demo.Models.User;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -26,50 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-
-        Observable<User> observable = retrofit
-                .create(GetUser.class)
-                .getuser("1")
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread());
-        
-        Observable<Post> observable2 = retrofit
-                .create(GetPost.class)
-                .getPost(1)
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread());
-               
-
-        Observable<UserAndPost> combined = Observable.zip(observable, observable2, (jsonObject, jsonObject1) -> new UserAndPost(jsonObject, jsonObject1));
-
-        combined.subscribe(new Observer<UserAndPost>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: ");
-            }
-
-            @Override
-            public void onNext(UserAndPost userAndEvents) {
-                Log.d(TAG, "onNext: "+ userAndEvents.post.getTitle() + ": "+ userAndEvents.user.getName());
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError: ");
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: ");
-            }
-        });
-        
     }
 }
